3ec7b0122cde869cd7788962bb660676c869b0eb
anzarhw <71497005+anzarhw@users.noreply.github.com>
2023-06-03 11:25:55 +0530

    first

---
+++ hugging-chat-book-bot-main/README.md
@@ -0,0 +1,5 @@
+# hugging-chat-book-bot
+A book recommendation bot. 
+
+Used [hugging-chat-api]([https://www.google.com](https://github.com/Soulter/hugging-chat-api))
+for frontend used streamlit framework
---
+++ hugging-chat-book-bot-main/app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Access the environment variables
+email = os.getenv('EMAIL')
+passwd = os.getenv('PASSWORD')
+
+sign = Login(email, passwd)
+cookies = sign.login()
+
+# Save cookies to usercookies/<email>.json
+sign.saveCookies()
+
+st.set_page_config(page_title="Book Recommendation Bot - An LLM-powered Streamlit bot")
+
+
+# header
+
+st.header("Hugging Chat - Book Recommendation Bot")
+
+st.divider()
+moods = st.multiselect(
+    'Select the mood',
+    ['Happy', 'Sad', 'Suspenseful', 'Scary', 'Romantic', 
+     'Adventure', 'Mystical', 'Funny', 'Inspirational', 
+     'Dramatic', 'Thoughtful', 'Thrilling', 
+     'Mysterious', 'Whimsical', 'Dark', "random"])
+
+st.write("")
+
+num_of_recom = st.selectbox(
+    'Select number of recommendations',
+    ('5', '10', '15'))
+
+st.write("")
+
+selected_moods = ' '.join(moods)
+
+query = "Suggest " + num_of_recom + "books based on the mood " + selected_moods + ". Just give a list of books, no details."
+
+def recom(query):
+    
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    response = chatbot.chat(query)
+    
+    return response
+
+gen = st.button("Generate")
+
+if gen:
+    try:
+        bot_response = recom(query)
+        st.text(bot_response)
+    except:
+        st.subheader("Please Try Again")
